6b8f79b9aefcf503a197f4f4a07bcdce9eeed3bb
909hehe <crypticahmed@gmail.com>
2024-02-16 22:20:12 +0530

    routes only

--- src/backend/routes/routes.js
+++ src/backend/routes/routes.js
@@ -1,73 +1,12 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('../test/db');
+const express = require("express");
+const router =  express.Router();
 
+const {fetchAllDataFromAnnouncement, fetchOneDataFromAnnouncement , fetchAllUsers, fetchOneUser} = require("../controllers");
 
-router.get('/announcements', async(req, res)=>{
+router.route("/announcements").get(fetchAllDataFromAnnouncement);
+router.route("/announcements/:id").get(fetchOneDataFromAnnouncement);
 
-    try {
-        
-        const all_ancs = await pool.query("SELECT * FROM announcements");
-        res.json(all_ancs.rows);
-
-    } catch (error) {
-        
-        console.log(error.message);
-        res.status(423).send("Currently unavailable");
-
-    }
-
-})
-
-router.get('/announcements/:id', async(req, res)=>{
-
-    try {
-        
-        const { id } = req.params;
-        const ancs_id = await pool.query("SELECT * FROM announcements WHERE id = $1", [id]);
-        res.json(ancs_id.rows[0]);
-
-    } catch (error) {
-        
-        console.log(error.message);
-        res.status(404).send("NOT FOUND");
-
-    }
-
-})
-
-router.get('/users', async(req, res)=>{
-
-    try {
-        
-        const users = await pool.query("SELECT * FROM users");
-        res.json(users.rows);
-
-    } catch (error) {
-        
-        console.log(error.message);
-        res.status(401).send("Authentication required");
-
-    }
-
-})
-
-router.get('/users/:id', async(req, res)=>{
-
-    try {
-        
-        const { id } = req.params;
-        const users = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-        res.json(users.rows[0]);
-
-    } catch (error) {
-        
-        console.log(error.message);
-        res.status(404).send("NOT FOUND");
-
-    }
-
-})
-
+router.route("/users").get(fetchAllUsers);
+router.route("/users/:id").get(fetchOneUser);
 
 module.exports = router;
