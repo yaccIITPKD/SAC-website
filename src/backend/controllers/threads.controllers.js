5abf9ddd9283dcd3bc8f5ae6f20ecc0440ddff1e
Amithabh-A <112101004@smail.iitpkd.ac.in>
2024-02-18 15:02:24 +0530

    comments added.

--- src/backend/controllers/threads.controllers.js
+++ src/backend/controllers/threads.controllers.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 const threads = require('../models/threads');
 
+// find all attributes from table
 const getAllFromthreads = asyncHandler(async (req, res) => {
   const userData = await threads.findAll();
   res.json(userData);
 });
 
-// fetchone for a table : 
+// find one attribute from table based on id.
 const fetchOneDataFromAnnouncement async = (req, res) => {
   const id = req.params.id;
   const userData = await threads.findByPk(id);
