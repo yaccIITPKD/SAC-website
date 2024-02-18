bff7e525059821229cc2ae041ba6362186c9def6
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-02-18 12:48:16 +0530

    removed the router and only defined the functions

--- src/backend/controllers/announcements.controllers.js
+++ src/backend/controllers/announcements.controllers.js
@@ -2,8 +2,6 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { Announcements } = require("../model");
 
-const router = express.Router();
-
 // to fetch all announcements
 const fetchAllAnnouncements = asyncHandler(async (req, res) => {
 	// using findAll method in sequelize
@@ -11,18 +9,10 @@ const fetchAllAnnouncements = asyncHandler(async (req, res) => {
 	res.json(rows);
 });
 
-const getRequest_AllAnnouncements = (path) => {
-	router.get(path, fetchAllAnnouncements);
-};
-
 const fetchAnnouncementId = asyncHandler(async (req, res) => {
 	const id = req.params.id;
 	const rows = await Announcements.findByPk(id);
 	res.json(rows);
 });
 
-const getRequest_AnnouncementId = (path) => {
-	router.get(path, fetchAnnouncementId);
-};
-
-module.exports = { getRequest_AllAnnouncements, getRequest_AnnouncementId };
+module.exports = { fetchAllAnnouncements, fetchAnnouncementId };
