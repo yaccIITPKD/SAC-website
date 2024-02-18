97ca7aa7b6093d1fc1b3f15060b07208a2751c3d
Neeraj Krishna N <neerajnjps03@gmail.com>
2024-02-18 13:05:15 +0530

    removed controllers.js and added separate controllers for announements and users

--- src/backend/controllers/announcements.controllers.js
+++ src/backend/controllers/announcements.controllers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const { Announcements } = require("../model");
+const { Announcements } = require("../models/announcements");
 
 // to fetch all announcements
 const fetchAllAnnouncements = asyncHandler(async (req, res) => {
@@ -9,10 +9,13 @@ const fetchAllAnnouncements = asyncHandler(async (req, res) => {
 	res.json(rows);
 });
 
-const fetchAnnouncementId = asyncHandler(async (req, res) => {
+// to fetch a single row using primary key
+const fetchAnnouncementById = asyncHandler(async (req, res) => {
+	// assuming that id is passed as a parameter in request
 	const id = req.params.id;
+	// using findByPk method in sequelize
 	const rows = await Announcements.findByPk(id);
 	res.json(rows);
 });
 
-module.exports = { fetchAllAnnouncements, fetchAnnouncementId };
+module.exports = { fetchAllAnnouncements, fetchAnnouncementById };
