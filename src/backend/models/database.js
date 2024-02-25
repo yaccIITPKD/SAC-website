6254e15d5433ba73c091db4567f1ff230ab8948e
SyedMa3 <sammateen10@outlook.com>
2024-02-26 03:30:41 +0530

    switched to remote postgres server

--- src/backend/models/database.js
+++ src/backend/models/database.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require("sequelize");
 // authentication replace database by name of database  , and password by your postgres password
-const sequelize = new Sequelize("sac-website", "postgres", "password", {
-  host: "localhost",
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+  host: PGHOST,
   dialect: "postgres",
   port: 5432,
+  dialectOptions: {
+    ssl: {
+      require: true
+    },
+  },
 });
 module.exports = { sequelize };
